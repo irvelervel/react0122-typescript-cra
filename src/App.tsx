a7557e5a7ac97d7ca879889d9c664d83b02a0ca8
Stefano Casasola <ste.casasola@gmail.com>
2022-09-15 09:59:06 +0200

    Prima pausa, es. con Class Component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import MyClassComponent from './components/MyClassComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyClassComponent subTitle="React-0122" />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MyClassComponent.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+
+// MyClassComponent, come tutti i componenti React, di default si aspettano
+// di ricevere come props un oggetto vuoto, questa è la loro tipizzazione di default
+
+interface MyClassComponentProps {
+  subTitle: string
+}
+
+interface MyClassComponentState {
+  name: string
+}
+
+class MyClassComponent extends Component<
+  MyClassComponentProps,
+  MyClassComponentState
+> {
+  state = {
+    name: 'Flavio',
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello TypeScript!</h1>
+        <h3>{this.props.subTitle}</h3>
+        <p onClick={() => this.setState({ name: 'Stefano' })}>
+          {this.state.name.toUpperCase()}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default MyClassComponent
